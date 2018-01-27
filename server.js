de3faf4caa4c0ba1cb2ea88b8a75f5950eca5f12
louiaR <louitiani@gmail.com>
2018-01-27 21:02:58 +0000

    remove comments on server.js change content of contact.handler

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const app = express();
 const fs = require('fs');
 const postsJson = require('./data/posts.json')
 
-// The extensions 'html' allows us to serve file without adding .html at the end 
-// i.e /my-cv will server /my-cv.html
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(express.static("public", {'extensions': ['html']}));
@@ -13,13 +11,10 @@ app.use(express.static("public", {'extensions': ['html']}));
 app.get("/", (req, res) => {
   const filePath = __dirname + "/data/posts.json";
   const callbackFunction = (error, file) => {
-    // we call .toString() to turn the file buffer to a String
     const fileData = file.toString();
-    // we use JSON.parse to get an object out the String
     const postsJson = JSON.parse(fileData);
-    // send the json to the Template to render
     res.render("index", {
-      title: "Louis Profile", // insert your name instead
+      title: "Louis Profile",
       posts: postsJson
     });
   };
